a585442fa0a189bf37d577515f3d194cdb5bb0fc
Alexandre <73314043+Alexander7474@users.noreply.github.com>
2020-10-22 22:53:55 +0200

    Add files via upload

---
+++ js/out-park.js
@@ -0,0 +1,6 @@
+var randomArray = ["whip", "bar", "heel", "bartwist", "finger-whip", "bri", "double-whip", "double-heel", "bar-whip", "bar-heel", "whip-bar", "heel-whip", "butter-cup", "umbrella",""];
+var randomItem = randomArray[Math.floor(Math.random() * randomArray.length)];
+document.write("Tricks = "+randomItem+".");
+
+
+
